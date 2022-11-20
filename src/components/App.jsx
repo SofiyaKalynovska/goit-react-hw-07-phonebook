86850a4e0e87e7559ea154e915433402e6b20723
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-11-20 22:45:45 +0200

    Refactoring App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,18 @@
-import { useState, useEffect } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Section from '../components/Section/Section';
 import Filter from './Fitler/Filter';
 
-export default function App() {
-  const [contacts, setContacts] = useState(() => {
-    return JSON.parse(window.localStorage.getItem('contacts') ?? '');
-  });
-  const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const handleContacts = ({ id, name, number }) => {
-    if (contacts.some(e => e.name === name)) {
-      alert(`${name} is alredy in contacts`);
-    } else {
-      setContacts([...contacts, { id, name, number }]);
-    }
-  };
-
-  const renderContactsList = () => {
-    return filter === ''
-      ? contacts
-      : contacts.filter(
-          contact =>
-            contact.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1
-        );
-  };
-
-  const handleFilter = evt => {
-    evt.preventDefault();
-    setFilter(evt.target.value);
-  };
-
-  const deleteContach = id => {
-    setContacts(contacts.filter(contact => contact.id !== id));
-  };
-
+export const App = () => {
   return (
     <>
       <Section title="Phonebook">
-        <ContactForm onSubmit={handleContacts} />
+        <ContactForm />
       </Section>
       <Section title="Contacts">
-        <Filter handleFilter={handleFilter} />
-        <ContactList
-          contactsList={renderContactsList()}
-          deleteContact={deleteContach}
-        />
+        <Filter />
+        <ContactList />
       </Section>
     </>
   );
