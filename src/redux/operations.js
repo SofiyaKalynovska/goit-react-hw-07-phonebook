ae901e74ac8f3736290bf1912445931fba4e4615
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-11-27 20:33:34 +0200

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -30,7 +30,8 @@ export const deleteContact = createAsyncThunk(
   "contacts/deleteContact",
   async (contactId, thunkAPI) => {
     try {
-      await axios.delete(`/contacts/${contactId}`);
+      const response = await axios.delete(`/contacts/${contactId}`);
+      return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
     }
